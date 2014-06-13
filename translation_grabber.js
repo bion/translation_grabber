48d52ab6f16dd768d9a69eb50dd0326a57421380
bion <bionjohnson@gmail.com>
2014-06-13 00:11:00 -0700

    Add guarded body request function

--- translation_grabber.js
+++ translation_grabber.js
@@ -6,8 +6,18 @@ var ORIGINAL_TEXT_QUERY_STRING = "?SHOW_ORIGINAL_TEXT";
 var INDEX_PAGE = 'http://watchingamerica.com/News/author/johnson2/';
 
 // request translator index page
+function bodyRequest(url, success) {
+    request(url, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            success(body);
+        } else {
+            console.log("unsuccessful request made to " + url);
+            process.exit(1);
+        }
 
 // find list of translation anchors
+    })
+}
 
 // for each anchor:
 //   pull down page, save contents of 'div.headcol1' to file
